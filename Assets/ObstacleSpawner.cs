50986934fc6169096c36a6069f2f62408f5b68eb
Khairul-hub <64126807+Khairul-hub@users.noreply.github.com>
2020-06-22 11:28:25 +0800

    Updated Unity Project

---
+++ Assets/ObstacleSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+    public GameObject[] obstacles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", 1f, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnObstacle()
+    {
+        int interger = Random.Range(0, obstacles.Length);
+        GameObject spawnObject = obstacles[interger];
+
+        Vector3 spawnPos = new Vector3(Random.Range(-1.5f, 1.5f), transform.position.y, transform.position.z);
+
+        Instantiate(spawnObject, spawnPos, Quaternion.identity);
+    }
+}
